faa6a993bdec5870da2216f55c8843adaf30725b
Daniel Hu <dh37@princeton.edu>
2023-12-01 00:56:06 -0500

    lmao

---
+++ src/graph_class.py
@@ -0,0 +1,238 @@
+import numpy as np
+from scipy.spatial import distance
+
+import heapq
+import random
+from typing import List, Tuple
+from tqdm import tqdm
+
+
+class Node:
+    """
+    Node for a navigable small world graph.
+
+    Parameters
+    ----------
+    idx : int
+        For uniquely identifying a node.
+
+    value : 1d np.ndarray
+        To access the embedding associated with this node.
+
+    neighborhood : set
+        For storing adjacent nodes.
+
+    References
+    ----------
+    https://book.pythontips.com/en/latest/__slots__magic.html
+    https://hynek.me/articles/hashes-and-equality/
+    """
+
+    __slots__ = ["idx", "value", "neighborhood"]
+
+    def __init__(self, idx, value):
+        self.idx = idx
+        self.value = value
+        self.neighborhood = set()
+
+    def __hash__(self):
+        return hash(self.idx)
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.idx == other.idx
+
+
+class Graph:
+    """type: "nsw-greedy","""
+
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
+
+    def greedy_search(
+        self, graph: List[Node], query: np.ndarray, k: int = 5, m: int = 50
+    ) -> Tuple[List[Tuple[float, int]], float]:
+        """
+        Performs knn search using the navigable small world graph.
+
+        Parameters
+        ----------
+        graph :
+            Navigable small world graph from build_nsw_graph.
+
+        query : 1d np.ndarray
+            Query embedding that we wish to find the nearest neighbors.
+
+        k : int
+            Number of nearest neighbors returned.
+
+        m : int
+            The recall set will be chosen from m different entry points.
+
+        Returns
+        -------
+        The list of nearest neighbors (distance, index) tuple.
+        and the average number of hops that was made during the search.
+        """
+        result_queue = []
+        visited_set = set()
+
+        hops = 0
+        for _ in range(m):
+            # random entry point from all possible candidates
+            entry_node = random.randint(0, len(graph) - 1)
+            entry_dist = distance.cosine(query, graph[entry_node].value)
+            candidate_queue = []
+            heapq.heappush(candidate_queue, (entry_dist, entry_node))
+
+            temp_result_queue = []
+            while candidate_queue:
+                candidate_dist, candidate_idx = heapq.heappop(candidate_queue)
+
+                # if candidate is further than the k-th (furthest) element from the result,
+                # then we would break the repeat loop
+                if len(result_queue) >= k:
+                    current_k_dist, current_k_idx = heapq.nsmallest(k, result_queue)[-1]
+                    if candidate_dist > current_k_dist:
+                        break
+
+                # iterate over neighbors and add them to candidates and results
+                for friend_node in graph[candidate_idx].neighborhood:
+                    if friend_node not in visited_set:
+                        visited_set.add(friend_node)
+
+                        friend_dist = distance.cosine(query, graph[friend_node].value)
+                        heapq.heappush(candidate_queue, (friend_dist, friend_node))
+                        heapq.heappush(temp_result_queue, (friend_dist, friend_node))
+                        hops += 1
+
+            result_queue = list(heapq.merge(result_queue, temp_result_queue))
+
+        return heapq.nsmallest(k, result_queue), hops / m
+
+    def build_nsw_greedy(self, index_factors: np.ndarray, k: int) -> List[Node]:
+        """
+        Builds a Navigable Small World (NSW) graph using a greedy approach.
+
+        This function iteratively adds nodes to the graph. Each node is connected to its k nearest neighbors
+        in the graph based on the distance between their respective high-dimensional data points.
+        For the first k nodes, they are connected to all previously added nodes.
+        The function uses a greedy search to find the nearest neighbors for each node.
+
+        Parameters
+        ----------
+        index_factors : np.ndarray
+            An array of high-dimensional data points. Each data point is used to create a node in the NSW graph.
+
+        k : int
+            The number of nearest neighbors to connect to each node in the graph.
+
+        Returns
+        -------
+        List[Node]
+            A list of Node objects, representing the nodes in the NSW graph. Each node contains information
+            about its index, its high-dimensional value, and a set of indices representing its neighbors.
+
+        Notes
+        -----
+        - The greedy search method used for finding nearest neighbors is dependent on the implementation
+            of the `greedy_search` method in the same class.
+        - The function provides a progress bar for tracking the graph construction process, using `tqdm`.
+        - Bi-directional connections are established between each node and its neighbors.
+
+        Examples
+        --------
+        >>> graph = Graph(type='nsw-greedy', data=some_data_array)
+        >>> nsw_graph = graph.build_nsw_greedy(index_factors=some_data_array, k=5)
+        """
+        tqdm_loader = tqdm(index_factors)
+        tqdm_loader.set_description("Building Graph")
+        graph = []
+        for i, value in enumerate(tqdm_loader):
+            node = Node(i, value)
+            # if we already have more than k nodes in the graph, attach to the
+            # k nearest neihgbors, found by greedy search
+            if i > k:
+                neighbors, hops = self.greedy_search(graph, node.value, k)
+                neighbors_indices = [node_idx for _, node_idx in neighbors]
+            else:
+                neighbors_indices = list(range(i))
+
+            # insert bi-directional connection
+            node.neighborhood.update(neighbors_indices)
+            for i in neighbors_indices:
+                graph[i].neighborhood.add(node.idx)
+
+            graph.append(node)
+
+        return graph
+
+    def beam_search(
+        self,
+        graph: List[Node],
+        query: np.ndarray,
+        k: int = 5,
+        m: int = 50,
+        beam_width: int = 10,
+    ) -> Tuple[List[Tuple[float, int]], float]:
+        """
+        Performs knn search using beam search on the navigable small world graph.
+
+        Parameters
+        ----------
+        graph :
+            Navigable small world graph from build_nsw_graph.
+
+        query : 1d np.ndarray
+            Query embedding that we wish to find the nearest neighbors.
+
+        k : int
+            Number of nearest neighbors returned.
+
+        m : int
+            The recall set will be chosen from m different entry points.
+
+        beam_width : int
+            Number of nodes to consider at each level of the search.
+
+        Returns
+        -------
+        The list of nearest neighbors (distance, index) tuple.
+        and the average number of hops that was made during the search.
+        """
+        result_queue = []
+        visited_set = set()
+
+        hops = 0
+        for _ in range(m):
+            entry_node = random.randint(0, len(graph) - 1)
+            entry_dist = distance.cosine(query, graph[entry_node].value)
+            candidate_queue = []
+            heapq.heappush(candidate_queue, (entry_dist, entry_node))
+
+            while candidate_queue:
+                temp_result_queue = []
+                # Consider up to beam_width best candidates
+                for _ in range(min(beam_width, len(candidate_queue))):
+                    candidate_dist, candidate_idx = heapq.heappop(candidate_queue)
+
+                    if len(result_queue) >= k:
+                        current_k_dist, _ = heapq.nsmallest(k, result_queue)[-1]
+                        if candidate_dist > current_k_dist:
+                            break
+
+                    for friend_node in graph[candidate_idx].neighborhood:
+                        if friend_node not in visited_set:
+                            visited_set.add(friend_node)
+                            friend_dist = distance.cosine(
+                                query, graph[friend_node].value
+                            )
+                            heapq.heappush(candidate_queue, (friend_dist, friend_node))
+                            heapq.heappush(
+                                temp_result_queue, (friend_dist, friend_node)
+                            )
+                            hops += 1
+
+                result_queue = list(heapq.merge(result_queue, temp_result_queue))
+
+        return heapq.nsmallest(k, result_queue), hops / m
---
+++ src/nsw.py
@@ -0,0 +1,195 @@
+import numpy as np
+from scipy.spatial import distance
+
+import pickle
+import heapq
+import random
+from typing import List, Tuple
+
+
+class Node:
+    """
+    Node for a navigable small world graph.
+
+    Parameters
+    ----------
+    idx : int
+        For uniquely identifying a node.
+
+    value : 1d np.ndarray
+        To access the embedding associated with this node.
+
+    neighborhood : set
+        For storing adjacent nodes.
+
+    References
+    ----------
+    https://book.pythontips.com/en/latest/__slots__magic.html
+    https://hynek.me/articles/hashes-and-equality/
+    """
+
+    __slots__ = ["idx", "value", "neighborhood"]
+
+    def __init__(self, idx, value):
+        self.idx = idx
+        self.value = value
+        self.neighborhood = set()
+
+    def __hash__(self):
+        return hash(self.idx)
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self.idx == other.idx
+
+class NSWGraph:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, data):
+        new_node = NSWNode(data)
+        for node in self.nodes:
+            # Connect new node with existing nodes if they are close
+            if self.should_connect(new_node, node):
+                new_node.neighbors.append(node)
+                node.neighbors.append(new_node)
+        self.nodes.append(new_node)
+
+    def should_connect(self, node1, node2, threshold=0.5):
+        # This function decides if two nodes should be connected.
+        # Threshold can be adjusted based on dataset characteristics.
+        distance = np.linalg.norm(node1.data - node2.data)
+        return distance < threshold
+
+    def greedy_search(self, query, max_steps=100):
+        if not self.nodes:
+            return None
+
+        current = np.random.choice(self.nodes)
+        for _ in range(max_steps):
+            if current is None:
+                break
+            closest = min(current.neighbors + [current], key=lambda node: np.linalg.norm(node.data - query))
+            if closest == current:
+                break
+            current = closest
+        return current
+
+    def beam_search(self, query, beam_width=2, max_steps = 100):
+        if not self.nodes:
+            return None
+
+        current = np.random.choice(self.nodes)
+        for _ in range(max_steps):
+            if current is None:
+                break
+            closest = min(current.neighbors + [current], key=lambda node: np.linalg.norm(node.data - query))
+            if closest == current:
+                break
+            current = closest
+        return current
+
+
+def greedy_search(
+    graph: List[Node], query: np.ndarray, k: int = 5, m: int = 50
+) -> Tuple[List[Tuple[float, int]], float]:
+    """
+    Performs knn search using the navigable small world graph.
+
+    Parameters
+    ----------
+    graph :
+        Navigable small world graph from build_nsw_graph.
+
+    query : 1d np.ndarray
+        Query embedding that we wish to find the nearest neighbors.
+
+    k : int
+        Number of nearest neighbors returned.
+
+    m : int
+        The recall set will be chosen from m different entry points.
+
+    Returns
+    -------
+    The list of nearest neighbors (distance, index) tuple.
+    and the average number of hops that was made during the search.
+    """
+    result_queue = []
+    visited_set = set()
+
+    hops = 0
+    for _ in range(m):
+        # random entry point from all possible candidates
+        entry_node = random.randint(0, len(graph) - 1)
+        entry_dist = distance.cosine(query, graph[entry_node].value)
+        candidate_queue = []
+        heapq.heappush(candidate_queue, (entry_dist, entry_node))
+
+        temp_result_queue = []
+        while candidate_queue:
+            candidate_dist, candidate_idx = heapq.heappop(candidate_queue)
+
+            if len(result_queue) >= k:
+                # if candidate is further than the k-th element from the result,
+                # then we would break the repeat loop
+                current_k_dist, current_k_idx = heapq.nsmallest(k, result_queue)[-1]
+                if candidate_dist > current_k_dist:
+                    break
+
+            for friend_node in graph[candidate_idx].neighborhood:
+                if friend_node not in visited_set:
+                    visited_set.add(friend_node)
+
+                    friend_dist = distance.cosine(query, graph[friend_node].value)
+                    heapq.heappush(candidate_queue, (friend_dist, friend_node))
+                    heapq.heappush(temp_result_queue, (friend_dist, friend_node))
+                    hops += 1
+
+        result_queue = list(heapq.merge(result_queue, temp_result_queue))
+
+    return heapq.nsmallest(k, result_queue), hops / m
+
+
+def build_nsw_graph(index_factors: np.ndarray, k: int) -> List[Node]:
+    n_nodes = index_factors.shape[0]
+
+    graph = []
+    for i, value in enumerate(index_factors):
+        node = Node(i, value)
+        if i > k:
+            neighbors, hops = greedy_search(graph, node.value, k)
+            neighbors_indices = [node_idx for _, node_idx in neighbors]
+        else:
+            neighbors_indices = list(range(i))
+
+        # insert bi-directional connection
+        node.neighborhood.update(neighbors_indices)
+        for i in neighbors_indices:
+            graph[i].neighborhood.add(node.idx)
+
+        graph.append(node)
+
+    return graph
+
+
+# now define a function to read the fvecs file format of Sift1M dataset
+def read_fvecs(fp):
+    a = np.fromfile(fp, dtype="int32")
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy().view("float32")
+
+
+def main():
+    base = read_fvecs("./siftsmall/siftsmall_base.fvecs")  # 1M samples
+    # also get some query vectors to search with
+    query = read_fvecs("./siftsmall/siftsmall_query.fvecs")
+    # take just one query (there are many in sift_learn.fvecs)
+    # xq = xq[0].reshape(1, xq.shape[1])
+    graph = build_nsw_graph(base, query)
+
+    with open("graph.pkl", "wb") as f:
+        pickle.dump(graph, f)
+
+
+if __name__ == "__main__":
+    main()
